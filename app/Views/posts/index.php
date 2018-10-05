92bbda7d211146420354f0011b9fde86976d3e51
maximekreber <maxime.kreber@gmail.com>
2018-10-05 03:33:54 +0200

    refonte front-end page show

--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -1,6 +1,6 @@
 <?php foreach ($posts as $post): ?>
 <div class="card mb-4">
-	<img class="card-img-top imagesarticles" src=".../../../public/img/alaska.jpg" alt="image article">
+	<img class="card-img-top" src=".../../../public/img/alaska.jpg" width="750" height="300" alt="image article">
     	<div class="card-body">
      		<h2 class="card-title"><?= $post->titre; ?></h2>
               	<p class="card-text"><?= $post->extrait; ?></p>
--- app/Views/posts/show.php
+++ app/Views/posts/show.php
@@ -1,16 +1,41 @@
-<h1><?= $article->titre; ?></h1>
+      <div class="row">
 
-<p><?= $article->contenu; ?></p>
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
 
-<p><?= $article->date; ?></p>
+          <!-- Title -->
+          <h1 class="mt-4"><?= $article->titre; ?></h1>
+
+          <!-- Author -->
+          <p class="lead">
+            par Jean Forteroche
+          </p>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <p>Posté le <?= $article->date; ?></p>
+
+          <hr>
+
+          <!-- Preview Image -->
+          <img class="rounded" src=".../../../public/img/alaska.jpg" width="900" height="300" alt="image article">
+
+          <hr>
+
+          <!-- Post Content -->
+          <p class="lead"><?= $article->contenu; ?></p>
+
+          <hr>
+      </div>
+  </div>
 
 <?php foreach($comment as $comments): ?>
-<p>Nom : <?= $comments->nom; ?></p>
 
-<p>Contenu : <?= $comments->contenu; ?></p>
-
-<p>Date : <?= $comments->date; ?> </p>
-
+	<div class="card my-4">
+              <h5 class="card-header"><?= $comments->nom;?> a posté un commentaire le <?= $comments->date;?></h5>
+              <div class="card-body">
+              <?= $comments->contenu; ?>
 <p> <?php if ($comments->signale !=0) {
      	?> <a class="btn btn-danger" href="?p=posts.signale&id=<?= $article->id; ?>&comments_id=<?= $comments->id; ?>">Commentaire Signalé</a> <?php
 
@@ -18,6 +43,9 @@
      	?> <a class="btn btn-primary" href="?p=posts.signale&id=<?= $article->id; ?>&comments_id=<?= $comments->id; ?> ">Signaler le Commentaire</a> <?php
 }
  ?></p>
+
+             </div>
+          </div>
 
 <?php endforeach; ?>
 
