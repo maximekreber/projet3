7b31fc63f42082d39f4bf16d0290c7e5a34a17be
maximekreber <maxime.kreber@gmail.com>
2018-10-05 06:04:08 +0200

    Ajout de la base de données avec les nouvelles colonne

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 24, 2018 at 11:26 AM
+-- Generation Time: Oct 05, 2018 at 04:01 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -24,18 +24,18 @@ CREATE TABLE `articles` (
   `id` int(11) NOT NULL,
   `titre` varchar(255) DEFAULT NULL,
   `contenu` longtext,
-  `date` datetime DEFAULT NULL
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `articles`
 --
 
-INSERT INTO `articles` (`id`, `titre`, `contenu`, `date`) VALUES
-(1, 'try again', 'try again', NULL),
-(2, 'try', 'try', NULL),
-(3, 'teste', 'teste', NULL),
-(4, 'try 22', 'try 2222', NULL);
+INSERT INTO `articles` (`id`, `titre`, `contenu`, `date`, `image`) VALUES
+(6, 'Fieri, inquam, Triari', '<p><span style=\"color: #5e5737; font-size: 10px;\">Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.</span></p>', '2018-10-05 05:42:38', 'https://static.lpnt.fr/images/2018/01/23/13016335lpw-13016796-article-alaska-jpg_4938432_660x281.jpg'),
+(7, 'Isdem diebus Apollinaris', '<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.</p>\r\n<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Isdem diebus Apollinaris Domitiani gener, paulo ante agens palatii Caesaris curam, ad Mesopotamiam missus a socero per militares numeros immodice scrutabatur, an quaedam altiora meditantis iam Galli secreta susceperint scripta, qui conpertis Antiochiae gestis per minorem Armeniam lapsus Constantinopolim petit exindeque per protectores retractus artissime tenebatur.</p>\r\n<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Soleo saepe ante oculos ponere, idque libenter crebris usurpare sermonibus, omnis nostrorum imperatorum, omnis exterarum gentium potentissimorumque populorum, omnis clarissimorum regum res gestas, cum tuis nec contentionum magnitudine nec numero proeliorum nec varietate regionum nec celeritate conficiendi nec dissimilitudine bellorum posse conferri; nec vero disiunctissimas terras citius passibus cuiusquam potuisse peragrari, quam tuis non dicam cursibus, sed victoriis lustratae sunt.</p>', '2018-10-05 05:47:59', 'https://i0.wp.com/www.coureurdesbois.fr/wp-content/uploads/2015/09/alaska-kayak-coureur-des-bois2.jpg?zoom=2.625&resize=402%2C201'),
+(8, 'Nam sole orto magnitudine', '<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Nam sole orto magnitudine angusti gurgitis sed profundi a transitu arcebantur et dum piscatorios quaerunt lenunculos vel innare temere contextis cratibus parant, effusae legiones, quae hiemabant tunc apud Siden, isdem impetu occurrere veloci. et signis prope ripam locatis ad manus comminus conserendas denseta scutorum conpage semet scientissime praestruebant, ausos quoque aliquos fiducia nandi vel cavatis arborum truncis amnem permeare latenter facillime trucidarunt.</p>\r\n<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>\r\n<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.</p>\r\n<p>&nbsp;</p>', '2018-10-05 05:52:50', 'https://www.canald.com/polopoly_fs/1.3517559.1513865161!/image/D_DecouvrezLAlaska_PhotoConcours_796x449.jpg_gen/derivatives/cd_796_449/D_DecouvrezLAlaska_PhotoConcours_796x449.jpg');
 
 -- --------------------------------------------------------
 
@@ -47,7 +47,8 @@ CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `contenu` longtext,
-  `date` datetime DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `signale` tinyint(1) NOT NULL,
   `articles_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -55,9 +56,16 @@ CREATE TABLE `comments` (
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `nom`, `contenu`, `date`, `articles_id`) VALUES
-(1, 'Maxime2', 'essai', NULL, 1),
-(4, 'test', 'test2', NULL, 1);
+INSERT INTO `comments` (`id`, `nom`, `contenu`, `date`, `signale`, `articles_id`) VALUES
+(9, 'John', '<p><span style=\"color: #5e5737; font-size: 10px;\">Itaque verae amicitiae difficillime reperiuntur in iis qui in honoribus reque publica versantur; ubi enim istum invenias qui honorem amici anteponat suo? Quid? Haec ut omittam, quam graves, quam difficiles plerisque videntur calamitatum societates! Ad quas non est facile inventu qui descendant. Quamquam Ennius recte.</span></p>', '2018-10-05 05:43:12', 0, 6),
+(10, 'Maxime', '<p><span style=\"color: #5e5737; font-size: 10px;\">Nihil morati post haec militares avidi saepe turbarum adorti sunt Montium primum, qui divertebat in proximo, levi corpore senem atque morbosum, et hirsutis resticulis cruribus eius innexis divaricaturn sine spiramento ullo ad usque praetorium traxere praefecti.</span></p>', '2018-10-05 05:44:35', 0, 6),
+(11, 'Richard', '<p><span style=\"color: #5e5737; font-size: 10px;\">Et Epigonus quidem amictu tenus philosophus, ut apparuit, prece frustra temptata, sulcatis lateribus mortisque metu admoto turpi confessione cogitatorum socium, quae nulla erant, fuisse firmavit cum nec vidisset quicquam nec audisset penitus expers forensium rerum; Eusebius vero obiecta fidentius negans, suspensus in eodem gradu constantiae stetit latrocinium illud esse, non iudicium clamans.</span></p>', '2018-10-05 05:46:03', 1, 6),
+(12, 'Maxime', '<p><span style=\"color: #5e5737; font-size: 10px;\">Accedebant enim eius asperitati, ubi inminuta vel laesa amplitudo imperii dicebatur, et iracundae suspicionum quantitati proximorum cruentae blanditiae exaggerantium incidentia et dolere inpendio simulantium, si principis periclitetur vita, a cuius salute velut filo pendere statum orbis terrarum fictis vocibus exclamabant.</span></p>', '2018-10-05 05:53:19', 0, 8),
+(13, 'Richard', '<p><span style=\"color: #5e5737; font-size: 10px;\">Sed si ille hac tam eximia fortuna propter utilitatem rei publicae frui non properat, ut omnia illa conficiat, quid ego, senator, facere debeo, quem, etiamsi ille aliud vellet, rei publicae consulere oporteret?</span></p>', '2018-10-05 05:53:38', 1, 8),
+(14, 'Maxime', '<p style=\"color: #5e5737; font-size: 10px; margin: 10px;\">Et est admodum mirum videre plebem innumeram mentibus ardore quodam infuso cum dimicationum curulium eventu pendentem. haec similiaque memorabile nihil vel serium agi Romae permittunt. ergo redeundum ad textum.</p>\r\n<p>&nbsp;</p>', '2018-10-05 05:56:52', 1, 7),
+(15, 'Richard', '<p><span style=\"color: #5e5737; font-size: 10px;\">Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit neque defendentibus nobis. Nam quod de pietate dixistis, est quidem ista nostra existimatio, sed iudicium certe parentis; quid nos opinemur, audietis ex iuratis; quid parentes sentiant, lacrimae matris incredibilisque maeror, squalor patris et haec praesens maestitia, quam cernitis, luctusque declarat.</span></p>', '2018-10-05 05:57:07', 0, 7),
+(16, 'Charles', '<p><span style=\"color: #5e5737; font-size: 10px;\">Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit neque defendentibus nobis. Nam quod de pietate dixistis, est quidem ista nostra existimatio, sed iudicium certe parentis; quid nos opinemur, audietis ex iuratis; quid parentes sentiant, lacrimae matris incredibilisque maeror, squalor patris et haec praesens maestitia, quam cernitis, luctusque declarat.</span></p>', '2018-10-05 05:57:26', 0, 7),
+(17, 'Charles', '<p><span style=\"color: #5e5737; font-size: 10px;\">Dein Syria per speciosam interpatet diffusa planitiem. hanc nobilitat Antiochia, mundo cognita civitas, cui non certaverit alia advecticiis ita adfluere copiis et internis, et Laodicia et Apamia itidemque Seleucia iam inde a primis auspiciis florentissimae.</span></p>', '2018-10-05 05:57:51', 0, 8);
 
 -- --------------------------------------------------------
 
@@ -109,13 +117,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
